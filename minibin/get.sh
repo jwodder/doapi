2e9b89cfedb574eb723f2bf002810e96933a9925
John T. Wodder II <jwodder@sdf.lonestar.org>
2015-10-29 15:47:44 +0000

    minibin/ is no longer necessary.

--- minibin/get.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-curl -sSL -X GET \
-	-H 'Content-Type: application/json' \
-	-H "Authorization: Bearer $(cat ~/.doapi)" \
-	"https://api.digitalocean.com${1:?Usage: $0 path}" | python -mjson.tool
