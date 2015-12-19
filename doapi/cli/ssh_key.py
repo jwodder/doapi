e052bbbe34b86d5ca1c4407a180e7c9a17d99ac1
John T. Wodder II <jwodder@sdf.lonestar.org>
2015-12-19 02:17:15 +0000

    doapi-ssh-key: Added `--unique` to `new` and `update`

--- doapi/cli/ssh_key.py
+++ doapi/cli/ssh_key.py
@@ -9,12 +9,14 @@ def main(argv=None, parsed=None):
     cmd_show = cmds.add_parser('show')
     cmd_show.add_argument('ssh_key', nargs='*')
     cmd_new = cmds.add_parser('new')
+    cmd_new.add_argument('--unique', action='store_true')
     cmd_new.add_argument('name')
     cmd_new.add_argument('pubkey', type=argparse.FileType('r'), nargs='?',
                          default=sys.stdin)
     cmd_delete = cmds.add_parser('delete')
     cmd_delete.add_argument('ssh_key', nargs='+')
     cmd_update = cmds.add_parser('update')
+    cmd_update.add_argument('--unique', action='store_true')
     cmd_update.add_argument('ssh_key')
     cmd_update.add_argument('name')
     args = parser.parse_args(argv, parsed)
@@ -25,12 +27,16 @@ def main(argv=None, parsed=None):
         else:
             util.dump(client.fetch_all_ssh_keys())
     elif args.cmd == 'new':
+        if args.unique and cache.name_exists("sshkey", args.name):
+            util.die('%s: name already in use' % (args.name,))
         util.dump(client.create_ssh_key(args.name, args.pubkey.read().strip()))
     elif args.cmd == 'delete':
         keys = cache.get_sshkeys(args.ssh_key, multiple=False)
         for k in keys:
             k.delete()
     elif args.cmd == 'update':
+        if args.unique and cache.name_exists("sshkey", args.name):
+            util.die('%s: name already in use' % (args.name,))
         key = cache.get_sshkey(args.ssh_key, multiple=False)
         util.dump(key.update_ssh_key(args.name))
     else:
