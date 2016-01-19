d65cb1100d40a2d9c79caa156225c49738c5756c
John T. Wodder II <jwodder@sdf.lonestar.org>
2016-01-19 16:24:10 +0000

    Added `description` fields to `doapi-ssh-key`

--- doapi/cli/ssh_key.py
+++ doapi/cli/ssh_key.py
@@ -4,10 +4,12 @@ from   . import _util as util
 
 def main(argv=None, parsed=None):
     parser = argparse.ArgumentParser(parents=[util.universal],
-                                     prog='doapi-ssh-key')
+                                     prog='doapi-ssh-key',
+                                     description='Manage DigitalOcean SSH keys')
     cmds = parser.add_subparsers(title='command', dest='cmd')
 
-    cmd_show = cmds.add_parser('show', help='List SSH keys')
+    cmd_show = cmds.add_parser('show', help='List SSH keys',
+                               description='List SSH keys')
     cmd_show.add_argument('-M', '--multiple', action='store_true',
                           help='Show multiple keys with the same name instead'
                                ' of erroring')
@@ -15,7 +17,8 @@ def main(argv=None, parsed=None):
                           help='ID, fingerprint, or name of a registered SSH'
                                ' key; omit to list all')
 
-    cmd_new = cmds.add_parser('new', help='Register an SSH public key')
+    cmd_new = cmds.add_parser('new', help='Register an SSH public key',
+                              description='Register an SSH public key')
     cmd_new.add_argument('--unique', action='store_true',
                          help='Error if the name is already in use')
     cmd_new.add_argument('name', help='name for the new SSH key')
@@ -24,7 +27,8 @@ def main(argv=None, parsed=None):
                          help='file containing an SSH public key'
                               ' (default: stdin)')
 
-    cmd_delete = cmds.add_parser('delete', help='Unregister an SSH key')
+    cmd_delete = cmds.add_parser('delete', help='Unregister an SSH key',
+                                 description='Unregister an SSH key')
     cmd_delete.add_argument('-M', '--multiple', action='store_true',
                             help='Delete multiple keys with the same name'
                                  ' instead of erroring')
@@ -32,7 +36,8 @@ def main(argv=None, parsed=None):
                             help='ID, fingerprint, or name of a registered SSH'
                                  ' key')
 
-    cmd_update = cmds.add_parser('update', help='Rename an SSH key')
+    cmd_update = cmds.add_parser('update', help='Rename an SSH key',
+                                 description='Rename an SSH key')
     cmd_update.add_argument('--unique', action='store_true',
                             help='Error if the new name is already in use')
     cmd_update.add_argument('ssh_key',
